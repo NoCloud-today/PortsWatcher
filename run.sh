6e44e3fc1b380b9e2281bbb3c13b4e086ce94051
mulyukovaaa <amina-mulukova.2002@yandex.ru>
2024-06-01 15:37:57 +0300

    remove check a blocking file to avoid restarting

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-if [ -f "running.txt" ]; then
-  echo "This script has already been run before" >&2
-  exit 1
-fi
-
-touch "running.txt"
 SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd $SCRIPTDIR
 source venv/bin/activate
 python3 nmap_monitor.py 2>&1 | tee -a scanner.log
-rm "running.txt"
